23b4fa33cf57168f729032ca9c606c474d36769b
ZZP-DMU <3571669089@qq.com>
2023-12-18 21:35:48 +0800

    some changes

--- src/SPH/SPH.cpp
+++ src/SPH/SPH.cpp
@@ -5,10 +5,13 @@ using namespace std;
 
 int main(void)
 { 
+    //get_max_men();
     SPH_PARTICLE particle;
     SPH_KERNEL kernel;
     SPH_PARE pare;
 
+
+    /************stack is too small***************
     double x[PTC_TOL_NUM]={0};
     double y[PTC_TOL_NUM]={0};
     double vx[PTC_TOL_NUM]={0};
@@ -16,13 +19,16 @@ int main(void)
     double pressure[PTC_TOL_NUM]={0};
     double density[PTC_TOL_NUM]={0};
     char type[PTC_TOL_NUM]={0};
+    ***********************************************/
 
-    particle.x = x;
-    particle.y = y;
-    particle.vx = vx;
-    particle.vy = vy;
-    particle.pressure = pressure;
-    particle.type = type;  
+   
+
+    particle.x = (double *)(malloc(sizeof(double)*PTC_TOL_NUM));
+    particle.y = (double *)(malloc(sizeof(double)*PTC_TOL_NUM));
+    particle.vx = (double *)(malloc(sizeof(double)*PTC_TOL_NUM));
+    particle.vy = (double *)(malloc(sizeof(double)*PTC_TOL_NUM));
+    particle.pressure = (double *)(malloc(sizeof(double)*PTC_TOL_NUM));
+    particle.type = (char *)(malloc(sizeof(char)*PTC_TOL_NUM));  
     ptc_generate(particle.x,particle.y);
 
     ofstream writefile;
