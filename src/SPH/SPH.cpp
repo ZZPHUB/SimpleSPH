af7c78a02a99c66e4c67205e2e7264cfa645675e
ZZP-DMU <3571669089@qq.com>
2023-12-19 17:23:40 +0800

    some work

--- src/SPH/SPH.cpp
+++ src/SPH/SPH.cpp
@@ -14,9 +14,25 @@ int main(void)
     particle.y = (double *)(malloc(sizeof(double)*PTC_TOL_NUM));
     particle.vx = (double *)(malloc(sizeof(double)*PTC_TOL_NUM));
     particle.vy = (double *)(malloc(sizeof(double)*PTC_TOL_NUM));
+    particle.accx = (double *)(malloc(sizeof(double)*PTC_TOL_NUM)); 
+    particle.accy = (double *)(malloc(sizeof(double)*PTC_TOL_NUM));
+    particle.density = (double *)(malloc(sizeof(double)*PTC_TOL_NUM));
+    particle.mass = (double *)(malloc(sizeof(double)*PTC_TOL_NUM));
+    particle.dif_density = (double *)(malloc(sizeof(double)*PTC_TOL_NUM));
     particle.pressure = (double *)(malloc(sizeof(double)*PTC_TOL_NUM));
     particle.type = (char *)(malloc(sizeof(char)*PTC_TOL_NUM));  
+
+    pair.i = (int *)(malloc(sizeof(int)*4*PTC_TOL_NUM));
+    pair.j = (int *)(malloc(sizeof(int)*4*PTC_TOL_NUM));
     ptc_generate(&particle);//generate the fluid solid and dummy particles
+
+    kernel.w = (double *)(malloc(sizeof(double)*4*PTC_TOL_NUM));
+    kernel.dwdx = (double *)(malloc(sizeof(double)*4*PTC_TOL_NUM));
+    kernel.dwdy = (double *)(malloc(sizeof(double)*4*PTC_TOL_NUM));
+
+    ptc_kernel(&particle,&pair,&kernel);
+    cout << "total interact particle num is " << pair.total << endl;
+
     ptc_init(&particle); //particles init values
 
     ofstream writefile;
@@ -34,6 +50,8 @@ int main(void)
     }
     
     writefile.close();
+
+
     return 0;
 }
 
