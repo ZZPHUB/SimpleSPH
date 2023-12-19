3c09ba0acb51f5fa60ab60a9cc69ff7a5d97e6e2
ZZP-DMU <3571669089@qq.com>
2023-12-19 22:29:27 +0800

    parallel setting

--- src/Lib/nnps.cpp
+++ src/Lib/nnps.cpp
@@ -4,16 +4,19 @@ using namespace std;
 void nnps_direct(SPH_PARTICLE *particle,SPH_PAIR *pair)
 {   
     pair->total = 0;
-    for(int i=0;i<PTC_TOL_NUM;i++)
+    #pragma omp parallel for num_threads(8)
     {
-        for(int j=i+1;j<PTC_TOL_NUM;j++)
+        for(int i=0;i<PTC_TOL_NUM;i++)
         {
-        /*  if the distance between the particles i and j is less or equ to PTC_RADIUS,then they are a pair*/
-            if(PTC_DISTANCE(i,j) <= PTC_REGION_RADIUS) 
+            for(int j=i+1;j<PTC_TOL_NUM;j++)
             {
-                pair->i[pair->total] = i;
-                pair->j[pair->total] = j;
-                pair->total = pair->total+1;
+            /*  if the distance between the particles i and j is less or equ to PTC_RADIUS,then they are a pair*/
+                if(PTC_DISTANCE(i,j) <= PTC_REGION_RADIUS) 
+                {
+                    pair->i[pair->total] = i;
+                    pair->j[pair->total] = j;
+                    pair->total = pair->total+1;
+                }
             }
         }
     }
--- src/Pre_Processing/ptc_init.cpp
+++ src/Pre_Processing/ptc_init.cpp
@@ -2,26 +2,15 @@
 
 void ptc_init(SPH_PARTICLE *particle)
 {
-    #pragma omp parallel sections num_threads(3)
+    #pragma omp parallel num_threads(8)
     {
-        #pragma omp section
+        #pragma omp for
         {
             for(int i=0;i<PTC_TOL_NUM;i++)
             {
-                particle->vx[i] = particle->vy[i] = particle->accx[i] = particle->accy[i] =particle->pressure[i] =particle->dif_density[i] = 0;
-            }
-        }
-        #pragma omp section
-        {
-            for(int i=0;i<PTC_TOL_NUM;i++)
-            {
+                particle->vx[i] = particle->vy[i] = particle->accx[i] = \
+                particle->accy[i] =particle->pressure[i] =particle->dif_density[i] = 0;
                 particle->mass[i] = 1000.0*pow(PTC_SPACING,3);
-            }
-        }
-        #pragma omp section
-        {
-            for(int i=0;i<PTC_TOL_NUM;i++)
-            {
                 particle->density[i] = 1000;
             }
         }
