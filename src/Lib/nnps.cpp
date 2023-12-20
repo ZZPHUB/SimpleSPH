ea3741bf169ce0df06495ea74bdfb3a5f2d0a15c
ZZP-DMU <3571669089@qq.com>
2023-12-20 12:40:03 +0800

    new

--- src/Lib/nnps.cpp
+++ src/Lib/nnps.cpp
@@ -6,10 +6,10 @@ void nnps_direct(SPH_PARTICLE *particle,SPH_PAIR *pair)
     pair->total = 0;
     #pragma omp parallel num_threads(7)
     {
-        #pragma omp for schedule(dynamic,PTC_TOL_NUM/140)
+        #pragma omp for schedule(dynamic,PTC_TOL_NUM/35)
         for(int i=0;i<PTC_TOL_NUM;i++)
         {   
-            if(omp_get_thread_num()==6) cout << "i is " << i << "thid is " << omp_get_thread_num() << endl;
+            //if(omp_get_thread_num()==6) cout << "i is " << i << "thid is " << omp_get_thread_num() << endl;
             for(int j=i+1;j<PTC_TOL_NUM;j++)
             {
             /*  if the distance between the particles i and j is less or equ to PTC_RADIUS,then they are a pair*/
@@ -18,6 +18,9 @@ void nnps_direct(SPH_PARTICLE *particle,SPH_PAIR *pair)
                     pair->i[pair->total] = i;
                     pair->j[pair->total] = j;
                     pair->total = pair->total+1;
+
+                    if(omp_get_thread_num()==6) cout << "total is " << pair->total << "thid is " << omp_get_thread_num() << endl;
+
                 }
             }
         }
