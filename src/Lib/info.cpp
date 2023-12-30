a93fa098e8c159e5e3fe6cc0816c1da267ac49f8
ZZP-DMU <3571669089@qq.com>
2023-12-30 21:25:07 +0800

    debug

--- src/Lib/info.cpp
+++ src/Lib/info.cpp
@@ -5,28 +5,28 @@ void ptc_info(SPH_PARTICLE *particle,SPH_PAIR *pair,RIGID *wedge,unsigned int ti
     system("clear");
     cout << "***************************SPH*************************" << endl;
     cout << "Total Particles Num: " << particle->total << endl;
-    cout << "------------------------------------------------------"
+    cout << "------------------------------------------------------" << endl;
     cout << "Fluid Particles Num: " << FLUID_PTC_NUM << endl;
-    cout << "------------------------------------------------------"
+    cout << "------------------------------------------------------" << endl;
     cout << "Virtual Particles Num: " << VIRTUAL_PTC_NUM << endl;
-    cout << "------------------------------------------------------"
+    cout << "------------------------------------------------------" << endl;
     cout << "Solid Particles Num: " << particle->total-FLUID_PTC_NUM-VIRTUAL_PTC_NUM << endl;
-    cout << "------------------------------------------------------"
+    cout << "------------------------------------------------------" << endl;
     cout << "Total Particles Pair Num: " << pair->total << endl;
-    cout << "------------------------------------------------------"
+    cout << "------------------------------------------------------" << endl;
     cout << "Current Time Step: " << time << endl;
-    cout << "------------------------------------------------------"
+    cout << "------------------------------------------------------" << endl;
     cout << "Rigid Body Velocity in X-direction: " << wedge->vx << endl;
-    cout << "------------------------------------------------------"
+    cout << "------------------------------------------------------" << endl;
     cout << "Rigid Body Velocity in Y-direction: " << wedge->vy << endl;
-    cout << "------------------------------------------------------"
+    cout << "------------------------------------------------------" << endl;
     cout << "Rigid Body Acceleration in X-direction: " << wedge->accx << endl;
-    cout << "------------------------------------------------------"
+    cout << "------------------------------------------------------" << endl;
     cout << "Rigid Body Acceleration in Y-direction: " << wedge->accy << endl;
-    cout << "------------------------------------------------------"
+    cout << "------------------------------------------------------" << endl;
     cout << "Rigid Body Angular Velocity: " << wedge->omega << endl;
-    cout << "------------------------------------------------------"
+    cout << "------------------------------------------------------" << endl;
     cout << "Rigid Body Angular Acceleration: "<< wedge->alpha << endl;
-    cout << "******************************************************"
+    cout << "******************************************************" << endl;
 
 }
