52522904086b73f165c73df08a0c19d7ae8defac
ZZP-DMU <3571669089@qq.com>
2024-01-04 10:32:27 +0800

    debug

--- src/Equations/accelerate.cpp
+++ src/Equations/accelerate.cpp
@@ -30,21 +30,21 @@ void ptc_acc(SPH_PARTICLE *particle,SPH_PAIR *pair,SPH_KERNEL *kernel)
         //to reduce the calculate tiems,define $p_i/\rho_i^2 + p_j/\rho_j^2$ to temp para
         temp_p = particle->pressure[pair->i[i]]/temp_rho_i + particle->pressure[pair->j[i]]/temp_rho_j;
 
-        particle->accx[pair->i[i]] = particle->accx[pair->i[i]] - temp_p*kernel->dwdx[i] + \
-        MU*(particle->visxx[pair->i[i]]/temp_rho_i + particle->visxx[pair->j[i]]/temp_rho_j)*kernel->dwdx[i] + \
-        MU*(particle->visxy[pair->i[i]]/temp_rho_i + particle->visxy[pair->j[i]]/temp_rho_j)*kernel->dwdy[i];
+        particle->accx[pair->i[i]] = particle->accx[pair->i[i]] - m*temp_p*kernel->dwdx[i] + \
+        MU*m*(particle->visxx[pair->i[i]]/temp_rho_i + particle->visxx[pair->j[i]]/temp_rho_j)*kernel->dwdx[i] + \
+        MU*m*(particle->visxy[pair->i[i]]/temp_rho_i + particle->visxy[pair->j[i]]/temp_rho_j)*kernel->dwdy[i];
 
-        particle->accx[pair->j[i]] = particle->accx[pair->j[i]] + temp_p*kernel->dwdx[i] - \
-        MU*(particle->visxx[pair->i[i]]/temp_rho_i + particle->visxx[pair->j[i]]/temp_rho_j)*kernel->dwdx[i] - \
-        MU*(particle->visxy[pair->i[i]]/temp_rho_i + particle->visxy[pair->j[i]]/temp_rho_j)*kernel->dwdy[i];
+        particle->accx[pair->j[i]] = particle->accx[pair->j[i]] + m*temp_p*kernel->dwdx[i] - \
+        MU*m*(particle->visxx[pair->i[i]]/temp_rho_i + particle->visxx[pair->j[i]]/temp_rho_j)*kernel->dwdx[i] - \
+        MU*m*(particle->visxy[pair->i[i]]/temp_rho_i + particle->visxy[pair->j[i]]/temp_rho_j)*kernel->dwdy[i];
 
-        particle->accy[pair->i[i]] = particle->accy[pair->i[i]] - temp_p*kernel->dwdy[i] + \
-        MU*(particle->visyy[pair->i[i]]/temp_rho_i + particle->visyy[pair->j[i]]/temp_rho_j)*kernel->dwdy[i] + \
-        MU*(particle->visxy[pair->i[i]]/temp_rho_i + particle->visxy[pair->j[i]]/temp_rho_j)*kernel->dwdx[i];
+        particle->accy[pair->i[i]] = particle->accy[pair->i[i]] - m*temp_p*kernel->dwdy[i] + \
+        MU*m*(particle->visyy[pair->i[i]]/temp_rho_i + particle->visyy[pair->j[i]]/temp_rho_j)*kernel->dwdy[i] + \
+        MU*m*(particle->visxy[pair->i[i]]/temp_rho_i + particle->visxy[pair->j[i]]/temp_rho_j)*kernel->dwdx[i];
 
-        particle->accy[pair->j[i]] = particle->accy[pair->j[i]] + temp_p*kernel->dwdy[i] - \
-        MU*(particle->visyy[pair->i[i]]/temp_rho_i + particle->visyy[pair->j[i]]/temp_rho_j)*kernel->dwdy[i] - \
-        MU*(particle->visxy[pair->i[i]]/temp_rho_i + particle->visxy[pair->j[i]]/temp_rho_j)*kernel->dwdx[i];
+        particle->accy[pair->j[i]] = particle->accy[pair->j[i]] + m*temp_p*kernel->dwdy[i] - \
+        MU*m*(particle->visyy[pair->i[i]]/temp_rho_i + particle->visyy[pair->j[i]]/temp_rho_j)*kernel->dwdy[i] - \
+        MU*m*(particle->visxy[pair->i[i]]/temp_rho_i + particle->visxy[pair->j[i]]/temp_rho_j)*kernel->dwdx[i];
 
         omp_unset_lock(&lock);
     }
