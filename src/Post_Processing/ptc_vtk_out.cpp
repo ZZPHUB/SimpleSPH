63e271c9771890f5c7f46f9f0605610046947cd0
ZZP-DMU <3571669089@qq.com>
2024-03-01 20:43:37 +0800

    add a function to save the last step

--- src/Post_Processing/ptc_vtk_out.cpp
+++ src/Post_Processing/ptc_vtk_out.cpp
@@ -2,7 +2,7 @@
 using namespace std;
 
 
-void ptc_vtk_direct(SPH *sph,char *filename)
+void sph_save_single(SPH *sph,char *filename)
 {
     SPH_PARTICLE *particle;
     particle = sph->particle;
@@ -69,7 +69,78 @@ void ptc_vtk_direct(SPH *sph,char *filename)
             << 0.0 << endl;
         }
     }
-
     writefile.close();
 
+}
+
+
+void sph_save_last(SPH *sph)
+{
+    SPH_PARTICLE *particle;
+    particle = sph->particle;
+
+    unsigned int ptc_num = 0;
+
+    ptc_num = particle->total;
+
+    ofstream writefile;
+    writefile.open("../data/postprocess/save.vtk");
+
+    writefile << "# vtk DataFile Version 3.0" << endl;
+    writefile << "sph data" << endl;
+    writefile << "ASCII" << endl;
+    writefile << "DATASET UNSTRUCTURED_GRID" << endl;
+    writefile << "POINTS " << ptc_num << " " << "double" << endl;
+
+    for(unsigned int i=0;i<particle->total;i++)
+    {
+        writefile << setiosflags(ios::scientific) << particle->x[i] << " " \
+        << particle->y[i] << " " << 0.0 << endl;
+    }
+
+    writefile << "POINT_DATA" << " " << ptc_num << endl;
+
+    //ptc type
+    writefile << "SCALARS "<< "density int 1" << endl;
+    writefile << "LOOKUP_TABLE DEFAULT" << endl;
+    for(unsigned int i=0;i<particle->total;i++)
+    {
+        writefile  << particle->type[i] << endl;
+    }
+
+    //density
+    writefile << "SCALARS "<< "density double 1" << endl;
+    writefile << "LOOKUP_TABLE DEFAULT" << endl;
+    for(unsigned int i=0;i<particle->total;i++)
+    {
+        writefile << setiosflags(ios::scientific) << particle->density[i] << endl;
+    }
+
+    //pressure
+    writefile << "SCALARS "<< "pressure double 1" << endl;
+    writefile << "LOOKUP_TABLE DEFAULT" << endl;
+    for(unsigned int i=0;i<particle->total;i++)
+    {
+        writefile << setiosflags(ios::scientific) << particle->pressure[i] << endl;
+    }
+    
+    //velocity
+    writefile << "VECTORS "<< "velocity double" << endl;
+    for(unsigned int i=0;i<particle->total;i++)
+    {
+        writefile << setiosflags(ios::scientific) << particle->vx[i] <<" " << particle->vy[i] << " " \
+        << 0.0 << endl;
+
+    }
+
+    //acceleration
+    writefile << "VECTORS "<< "acceleration double" << endl;
+    for(unsigned int i=0;i<particle->total;i++)
+    {
+        writefile << setiosflags(ios::scientific) << particle->accx[i] <<" " << particle->accy[i] << " " \
+        << 0.0 << endl;
+    }
+
+
+    writefile.close();
 }
--- src/SPH/SPH.cpp
+++ src/SPH/SPH.cpp
@@ -24,28 +24,20 @@ int main(void)
     
     char filename[] = "../data/postprocess/vtk/sph000.vtk"; //filename 
 
-    while (true)
+    for(sph.current_step;sph.current_step<sph.total_step;sph.current_step++)
     {
-        for(sph.current_step;sph.current_step<sph.total_step;sph.current_step++)
+        if(sph.current_step%PRINT_TIME_STEP == 0)
         {
-            if(sph.current_step%PRINT_TIME_STEP == 0)
-            {
-                filename[27] = (sph.current_step/PRINT_TIME_STEP)/100 + 48;
-                filename[28] = ((sph.current_step/PRINT_TIME_STEP)%100)/10 + 48;
-                filename[29] = ((sph.current_step/PRINT_TIME_STEP)%10) + 48;
-                ptc_vtk_direct(&sph,filename);
-            }
-            //calculate and integration
-            sph_time_integral(&sph); 
-            ptc_info(&sph);
-            ptc_pressure_conv(&sph);
+            filename[27] = (sph.current_step/PRINT_TIME_STEP)/100 + 48;
+            filename[28] = ((sph.current_step/PRINT_TIME_STEP)%100)/10 + 48;
+            filename[29] = ((sph.current_step/PRINT_TIME_STEP)%10) + 48;
+            sph_save_single(&sph,filename);
         }
-        system("clear");
-        cout << "press 0 to kill precess or num(>100) for more steps" << endl;
-        cin >> sph.total_step;
-        if(sph.total_step == 0) break;
+        //calculate and integration
+        sph_time_integral(&sph); 
+        ptc_info(&sph);
     }
-
+    sph_save_last(&sph);
     sph_free(&sph);
     return 0;
 }
