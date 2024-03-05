28db9dde7155c6b089d503b3b62cf19a356a1de5
ZZP-DMU <3571669089@qq.com>
2024-03-05 15:30:23 +0800

    debug

--- src/SPH/sph_time_int.cpp
+++ src/SPH/sph_time_int.cpp
@@ -109,9 +109,9 @@ void sph_rigid_integral(SPH *sph)
         {
             if(particle->type[i] == 1)
             {
-                wedge->accx -= particle->accx[i]*m/wedge->mass;
-                wedge->accy -= particle->accy[i]*m/wedge->mass;
-                wedge->alpha -= (particle->accy[i]*(particle->x[i]-wedge->cogx)-\
+                wedge->accx += particle->accx[i]*m/wedge->mass;
+                wedge->accy += particle->accy[i]*m/wedge->m ass;
+                wedge->alpha += (particle->accy[i]*(particle->x[i]-wedge->cogx)-\
                                  particle->accx[i]*(particle->y[i]-wedge->cogy))*m/wedge->mass;
             }
         }
