f802b7d6af4d93aa9c68522fa5fa54175b4be5d7
ZZP-DMU <3571669089@qq.com>
2024-04-05 12:42:10 +0800

    debug

--- src/SPH/sph_cuda.cu
+++ src/SPH/sph_cuda.cu
@@ -114,8 +114,8 @@ int main(void)
     dim3 ptc_block(256);
     dim3 ptc_grid((int)(sph.particle->total/256)+1);
     //define the seed for mesh data structure
-    dim3 mesh_block(64);
-    dim3 mesh_grid(MESH_LENGTH_NUM,MESH_DEEPTH_NUM,64);
+    dim3 mesh_block(32,32);
+    dim3 mesh_grid(MESH_LENGTH_NUM,MESH_DEEPTH_NUM);
     //define the seed for pair data structre
     dim3 pair_block(512);
     dim3 pair_grid((int)(sph.particle->total/16)+1);
