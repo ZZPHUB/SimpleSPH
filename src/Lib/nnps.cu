35a36fd4ded6c91fe6db4ddeef38ce98eabb90c0
ZZP-DMU <3571669089@qq.com>
2024-04-11 14:18:20 +0800

    debug

--- src/Lib/nnps.cu
+++ src/Lib/nnps.cu
@@ -68,8 +68,7 @@ __global__ void sph_nnps_cuda(SPH_CUDA *cuda,SPH_ARG *arg,SPH_RIGID *rigid)
     //mesh[x,y]->mesh[x,y+1]
     if( blockIdx.y < ( gridDim.y-1))
     {
-        mesh_near_num = mesh[ mesh_id + gridDim.x];
-        if( threadIdx.y< mesh_near_num )
+        if( threadIdx.y< cuda->mesh[ mesh_id + gridDim.x] )
         {
             j = mesh_id + gridDim.x + threadIdx.y*arg->mesh_num;
             q = (cuda->x[cuda->mesh[i]]-cuda->x[cuda->mesh[j]])*(cuda->x[cuda->mesh[i]]-cuda->x[cuda->mesh[j]])+(cuda->y[cuda->mesh[i]]-cuda->y[cuda->mesh[j]])*(cuda->y[cuda->mesh[i]]-cuda->y[cuda->mesh[j]]);
@@ -95,7 +94,6 @@ __global__ void sph_nnps_cuda(SPH_CUDA *cuda,SPH_ARG *arg,SPH_RIGID *rigid)
     //mesh[x,y]->mesh[x+1,y+1]
     if( blockIdx.x < ( gridDim.x-1) && blockIdx.y < ( gridDim.y-1))
     {
-        
         if( threadIdx.y< cuda->mesh_count[mesh_id + 1 + gridDim.x])
         {
             j = mesh_id + 1 + gridDim.x + threadIdx.y*arg->mesh_num;
@@ -129,7 +127,7 @@ __global__ void sph_nnps_cuda(SPH_CUDA *cuda,SPH_ARG *arg,SPH_RIGID *rigid)
             q = sqrt(q)/arg->h;
             if(q<2.0)
             {
-                if(cuda->type[cuda->cuda->mesh[i]] == 0)
+                if(cuda->type[cuda->mesh[i]] == 0)
                 {
                     count_temp = atomicAdd(&(arg->pair_num),1);
                     cuda->pair_i[count_temp] = cuda->mesh[i];
