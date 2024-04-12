1c0ab343cd25b7f0cdf16ceb0c7baf276864d7a2
ZZP-DMU <3571669089@qq.com>
2024-04-12 11:08:36 +0800

    debug

--- src/Lib/mesh.cu
+++ src/Lib/mesh.cu
@@ -15,6 +15,7 @@ __global__ void sph_mesh_cuda(SPH_CUDA *cuda,SPH_ARG *arg)
         //printf("the arg tmp is:%d the pair num is:%d \n",arg->tmp,arg->pair_num);
         arg->tmp = 0;
         arg->pair_num = 0;
+    }
 
     int mid = 0;
     int mesh_index = 0;
