4853affda310f319ca7e1560353f76c82e3ff99d
ZZP-DMU <3571669089@qq.com>
2024-04-19 11:38:44 +0800

    debug

--- src/Pre_Processing/sph_change_case.cu
+++ src/Pre_Processing/sph_change_case.cu
@@ -60,6 +60,7 @@ int main(int argc,char *argv[])
 
     tmp_sph.host_arg->case_dir = argv[2];
     tmp_arg.rigid_ptc_num = new_rigid_num(&tmp_sph);
+    tmp_arg.pair_volume = (int)(tmp_arg.ptc_num*32/tmp_arg.mesh_num);
     tmp_arg.ptc_num = tmp_arg.fluid_ptc_num + tmp_arg.wall_ptc_num + tmp_arg.rigid_ptc_num;
     tmp_particle.x = (double *)calloc(tmp_arg.ptc_num,sizeof(double));
     tmp_particle.y = (double *)calloc(tmp_arg.ptc_num,sizeof(double));
